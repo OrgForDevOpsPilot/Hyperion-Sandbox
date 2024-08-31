9145c59f9897d02c60a6a2c40c9af37bace9031f
yutaka.osada <yutaka.osada@sompodds.onmicrosoft.com>
2024-09-01 01:11:17 +0900

    Azure Artifacts Sample

--- src/DemoSite/Pages/Index.cshtml.cs
+++ src/DemoSite/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DemoCoreServices;     //・・・①
 
 namespace DemoSite.Pages;
 
@@ -9,6 +10,8 @@ public class IndexModel : PageModel
 
     [BindProperty]
     public string SystemMessageText { get; set; } = string.Empty;
+    [BindProperty]
+    public string ArtifactsLibraryText { get; set; } = string.Empty;    //・・・②
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -18,5 +21,7 @@ public class IndexModel : PageModel
     public void OnGet()
     {
         this.SystemMessageText = Environment.GetEnvironmentVariable("MESSAGE");
+        CoreFunctionality coreFunctionality = new CoreFunctionality();
+        this.ArtifactsLibraryText = coreFunctionality.Display();    //・・・③
     }
 }
